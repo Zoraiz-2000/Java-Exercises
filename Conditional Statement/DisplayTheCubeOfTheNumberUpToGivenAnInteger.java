efae49f01c5ee2057fb4b9466e92f3343055c87f
Zoraiz-2000 <zoraizabdurrehman@gmail.com>
2023-12-04 18:02:15 +0000

    13. Display The Cube Of The Number Up To Given An Integer

---
+++ Conditional Statement/DisplayTheCubeOfTheNumberUpToGivenAnInteger.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DisplayTheCubeOfTheNumberUpToGivenAnInteger {
+
+    //13. Write a Java program to display the cube of the given number up to an integer.
+    public static void main(String[] args) {
+
+        int i,n;
+
+        System.out.print("Input number of terms : ");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+
+        for(i=1;i<=n;i++)
+        {
+            System.out.println("Number is : " +i+" and cube of " +i+" is : "+(i*i*i));
+        }
+    }
+}
