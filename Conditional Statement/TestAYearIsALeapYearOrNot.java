8f8955e129800f470cc11198b926786f431b85c2
Zoraiz-2000 <zoraizabdurrehman@gmail.com>
2023-12-04 15:53:28 +0000

    9. Test A Year Is A Leap Year Or Not

---
+++ Conditional Statement/TestAYearIsALeapYearOrNot.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TestAYearIsALeapYearOrNot {
+
+    //9. Write a Java program that takes a year from the user and prints whether it is a leap year or not.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input the year: ");
+        int year = in.nextInt();
+
+        boolean x = (year % 4) == 0;
+        boolean y = (year % 100) != 0;
+        boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+        if (x && (y || z))
+        {
+            System.out.println(year + " is a leap year");
+        }
+        else
+        {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
