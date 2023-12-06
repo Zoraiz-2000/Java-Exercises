326b51618d25231e59fa56d2fc6a8b7a49948226
Zoraiz-2000 <zoraizabdurrehman@gmail.com>
2023-12-06 14:54:12 +0000

    2. Calculate The Sum Of Numbers From 1 To N

---
+++ Java Recursive Methods/SumCalculator.java
@@ -0,0 +1,19 @@
+public class SumCalculator {
+
+    //2. Write a Java recursive method to calculate the sum of all numbers from 1 to n.
+    public static int calculateSum(int n) {
+        // Base case: sum of 0 is 0
+        if (n == 0) {
+            return 0;
+        }
+
+        // Recursive case: add n with the sum of (n-1)
+        return n + calculateSum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int number = 7;
+        int sum = calculateSum(number);
+        System.out.println("Sum of numbers from 1 to " + number + " is: " + sum);
+    }
+}
