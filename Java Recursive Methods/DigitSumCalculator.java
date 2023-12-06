1d8248b10501fac621e88261ee2237bbadbaed9a
Zoraiz-2000 <zoraizabdurrehman@gmail.com>
2023-12-06 16:19:19 +0000

    14. Find The Sum Of Digits In An Integer

---
+++ Java Recursive Methods/DigitSumCalculator.java
@@ -0,0 +1,22 @@
+public class DigitSumCalculator {
+
+    //14. Write a Java recursive method to find the sum of the digits of a given integer.
+    public static int calculateDigitSum(int number) {
+        // Base case: if the number is a single digit, return the number itself
+        if (number < 10) {
+            return number;
+        }
+
+        // Recursive case: calculate the sum of the last digit and the digit sum of the remaining number
+        int lastDigit = number % 10;
+        int remainingNumber = number / 10;
+
+        return lastDigit + calculateDigitSum(remainingNumber);
+    }
+
+    public static void main(String[] args) {
+        int number = 123456;
+        int digitSum = calculateDigitSum(number);
+        System.out.println("The sum of the digits of " + number + " is: " + digitSum);
+    }
+}
